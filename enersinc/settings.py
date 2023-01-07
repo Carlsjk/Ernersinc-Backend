c6ce806fc11fdfb408a0e971f9593f00eba5a83d
Jhonatan <cumb1234@gmail.com>
2023-01-07 16:43:52 -0500

    add sqlite

--- enersinc/settings.py
+++ enersinc/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'enersinc.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE',"django.db.backends.postgresql"),
@@ -97,7 +97,7 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT',"5432"),
     },
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
